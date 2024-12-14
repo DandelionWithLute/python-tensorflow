d33d985ebb5ba4aaa098b135efe2aeebffb4543e
Dandelion <114991717+DandelionWithLute@users.noreply.github.com>
2024-12-14 15:07:28 +0800

    fashion_mnist

---
+++ matplot.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# https://www.w3schools.com/python/matplotlib_getting_started.asp
+xpoints = np.array([1, 8])
+ypoints = np.array([3, 10])
+plt.plot(xpoints, ypoints)
+plt.show()
